27e8db1aa27524c7f270dba50134e07cb631e0ad
RasmiRamani <114068933+RasmiRamani@users.noreply.github.com>
2022-10-13 16:51:47 +0530

    n=int(input()) s=0 a=n**2 q=a%10 w=a%100 r=a%1000 if n==q or n==w or n==r:    print('Automorphic Number') else:     print('Not an Automorphic Number')

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,11 +1,10 @@
 n=int(input())
 s=0
 a=n**2
-num=a%10
-num2=a%100
-num3=a%1000
-if n==num or n==num2 or n==num3:
-    print("Automorphic Number")
+q=a%10
+w=a%100
+r=a%1000
+if n==q or n==w or n==r:
+   print('Automorphic Number')
 else:
-    print("Not an Automorphic Number")
-     
+    print('Not an Automorphic Number')
